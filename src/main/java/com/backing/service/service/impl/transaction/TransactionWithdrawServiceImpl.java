8812b7133ee27050280f8ea82b69c68899cb1990
zaborskiy <zaborskiy_ai@mail.ru>
2023-10-08 19:17:03 +0300

    TODO транзакций

--- src/main/java/com/backing/service/service/impl/transaction/TransactionWithdrawServiceImpl.java
+++ src/main/java/com/backing/service/service/impl/transaction/TransactionWithdrawServiceImpl.java
@@ -11,6 +11,7 @@ import com.backing.service.service.TransactionService;
 import com.backing.service.util.BankAccountValidation;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
@@ -23,7 +24,10 @@ public class TransactionWithdrawServiceImpl implements TransactionService {
     private final TransactionHistoryRepository transactionHistoryRepository;
 
     @Override
-    @Transactional
+    //TODO есть подозрение, что тут как раз таки нужен REPEATABLE_READ,
+    // т.к. прочитанные данные все равно могут быть изменены паралельной транзакцией (тобишь проблема неповторяющегося чтения)
+    // попробовать решить эту проблему оптимистичной и пессимистичной блокировкой и разобраться как написать тесты
+    @Transactional(isolation = Isolation.REPEATABLE_READ)
     public void transaction(Long bankAccountId, TransactionRequestDto dto) {
         BankAccount bankAccount = bankAccountRepository.findById(bankAccountId).orElseThrow(() -> new EntityNotExistException(
                 String.format("BankAccount by id = %d not found", bankAccountId)
@@ -31,6 +35,8 @@ public class TransactionWithdrawServiceImpl implements TransactionService {
         BankAccountValidation.equalsPinCode(bankAccount.getPinCode(), dto.getPinCode());
         BankAccountValidation.checkBalance(bankAccount.getBalance(), dto.getPrice());
 
+        //TODO нужна ли тут транзакция? можем ли мы где то паралельно снять деньги и уйти в минус
+        //надо проверить этот код с разными уровнями транзакций
         bankAccount.minusBalance(dto.getPrice());
 
         TransactionHistory transactionHistory = TransactionHistory.builder()

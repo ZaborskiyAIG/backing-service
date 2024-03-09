79a34e8e6f605ea393383d72587feffa97b4b1ae
Александр <zaborskiyaig@gmail.com>
2024-03-09 22:49:43 +0300

    g

--- src/main/java/com/backing/service/client/FeignClientTest.java
+++ src/main/java/com/backing/service/client/FeignClientTest.java
@@ -4,10 +4,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "producer")
+@FeignClient(value = "producer", url = "/ping")
 public interface FeignClientTest {
 
 
-    @GetMapping("/ping")
+    @GetMapping
     String getPing();
 }

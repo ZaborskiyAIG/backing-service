4f02712f7628bad42067aa7baba1442ca9551767
Александр <zaborskiyaig@gmail.com>
2024-03-09 23:02:14 +0300

    g

--- src/main/java/com/backing/service/client/Producer.java
+++ src/main/java/com/backing/service/client/Producer.java
@@ -1,11 +1,10 @@
 package com.backing.service.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(value = "producer", url = "producer")
-public interface FeignClientTest {
+@FeignClient(value = "producerv2", url = "produce-svc")
+public interface Producer {
 
 
     @GetMapping("/ping")
--- src/main/java/com/backing/service/controller/TestRestController.java
+++ src/main/java/com/backing/service/controller/TestRestController.java
@@ -1,6 +1,7 @@
 package com.backing.service.controller;
 
 import com.backing.service.client.FeignClientTest;
+import com.backing.service.client.Producer;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestRestController {
 
     private final FeignClientTest feignClientTest;
+    private final Producer producer;
 
     @Operation(summary = "Далем запрос на другой сервис")
     @ApiResponses(value = {
@@ -30,5 +32,16 @@ public class TestRestController {
     public ResponseEntity<String> getService() {
         log.info("отправили запрос");
         return ResponseEntity.ok(feignClientTest.getPing());
+    }
+
+    @Operation(summary = "Далем запрос на другой сервис")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Получилось"),
+            @ApiResponse(responseCode = "400", description = "облажался")
+    })
+    @GetMapping
+    public ResponseEntity<String> getServiceV2() {
+        log.info("отправили запрос");
+        return ResponseEntity.ok(producer.getPing());
     }
 }

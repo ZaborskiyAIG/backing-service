dc80150106cebdb697b6ffdbec3fc3dfe5bb2191
Александр <zaborskiyaig@gmail.com>
2024-08-31 13:39:40 +0300

    что то

---
+++ src/main/java/com/backing/service/client/Dto.java
@@ -0,0 +1,14 @@
+package com.backing.service.client;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Dto {
+
+    String name;
+
+}

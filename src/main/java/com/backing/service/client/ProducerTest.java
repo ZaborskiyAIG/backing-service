a8758421b0d818779c47d2164e6571e2e542b28b
Александр <zaborskiyaig@gmail.com>
2025-06-12 15:51:46 +0300

    что то

--- src/main/java/com/backing/service/client/ProducerTest.java
+++ src/main/java/com/backing/service/client/ProducerTest.java
@@ -3,7 +3,7 @@ package com.backing.service.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(value = "producerv2", url = "my-java-service")
+@FeignClient(value = "producerv2", url = "my-java-service:8080")
 public interface ProducerTest {
 
 
